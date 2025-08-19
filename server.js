8ff7d4b304c09903f02a01c56db1500a0f92cb9f
Debdoot Manna <106653463+DebdootManna@users.noreply.github.com>
2025-08-19 11:07:55 +0530

    deleted unnecessary files

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("<br><h1>Welcome to My Server</h1>");
-  res.end('<h1>Hello World</h1><p>This is a simple HTTP server.</p>');
-}).listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
