b3d0bb0c9972eaef387fa91fd8bcc66f04171ebf
Debdoot Manna <106653463+DebdootManna@users.noreply.github.com>
2025-10-10 09:32:55 +0530

    practical 7

---
+++ practical7/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/reports' element={<Reports />} />
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
