4aaa2d36f454ebbf0abdc09786aed7f47375bc27
Debdoot Manna <106653463+DebdootManna@users.noreply.github.com>
2025-10-10 11:31:05 +0530

    practical 18

---
+++ practical18/app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var notesRouter = require('./routes/notes');
+
+var app = express();
+
+//- MongoDB connection
+const mongoURI = 'mongodb://localhost:27017/notes';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/notes', notesRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
+
+module.exports = app;
